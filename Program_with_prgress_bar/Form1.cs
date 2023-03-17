47aeeb6e5fe1e52f7cdaf8f3673b18bb4e6cbc30
Егор Корсун <egorkorsun7@gmail.com>
2023-03-17 17:11:33 +0100

    Добавьте файлы проекта.

---
+++ Program_with_prgress_bar/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_with_prgress_bar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+
+                    Thread.Sleep(25);
+
+                    progressBar1.PerformStep();
+             
+
+
+            }
+        }
+    }
+}
